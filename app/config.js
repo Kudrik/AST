739fef99f0c16f82b80ba7bbedec8ad7ae6098fe
Kudrik <kudrik.ru@gmail.com>
2015-09-21 20:42:57 +0700

    first commit

---
+++ app/config.js
@@ -0,0 +1,19 @@
+define(function () {
+    "use strict";
+
+    // Use strict JSON format
+    return {
+
+        "data":  {
+            "url": "/data.json"
+        },
+
+        "locations": {
+            "url": "/locations.json"
+        },
+
+        "debug": false
+
+    }
+
+});
